2a823224651f2cb37018b60df2488827e5a1eab3
devinkinh <kevtandinh@gmail.com>
2020-05-05 20:48:22 -0400

    some react fun

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.scss';
 
@@ -6,19 +7,29 @@ function App() {
 
   const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+  useEffect( () => {
+    async function fetchData()  {
+      let result = await fetch('/time');
+      result = await result.json()
+
+      setCurrentTime(result.time);
+    }
+    setInterval(()=>{
+      fetchData();    
+    },1000);
   }, []);
 
-
+  const getTime= function(time){
+    let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    d.setUTCSeconds(time);
+    return d+"";
+  }
   return (
     <div className="App">
       <header className="App-header">
         Kevin Dinh
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="accent">The current time is {currentTime}.</p>
+        <p className="accent">The current time is {getTime(currentTime)}.</p>
       </header>
     </div>
   );
